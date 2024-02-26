05719bd1ad38eb60cc26740cf0e2204951fb3b8b
liaozhangsheng <liaozhangsheng@163.com>
2024-02-26 08:14:15 +0800

    Update

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 from src.DHUPotatoPatch import DHUPotatoPatch
 
 # 初始化
-with open("cookie", "r") as f:
+with open("cookie", "r") as f: # 从教务系统获取cookie存入文件中再读取，或者直接填入也行
     cookie = f.read()
 bot = DHUPotatoPatch(cookie)
 
@@ -24,22 +24,22 @@ bot = DHUPotatoPatch(cookie)
 查询课程
 '''
 # 通过课程代码搜索课程
-print(bot.search_courses_by_id("039981"))
+print(await bot.search_courses_by_id("092161"))
 
 # 通过学院搜索课程
-print(bot.search_courses_by_collage(3))
+print(await bot.search_courses_by_collage(3))
 
 # 通过课程名搜索课程
-print(bot.search_courses_by_name("篮球"))
+print(await bot.search_courses_by_name("篮球"))
 
 '''
 选课、退课
 '''
 # 退课
-print(bot.remove_course("039981", 1))
+print(await bot.remove_course("039981", 1))
 
 # 选课
-print(bot.select_course("269663")) # 填课程序号
+print(await bot.select_course("269663")) # 填课程序号
 
 '''
 查成绩、绩点
@@ -55,25 +55,36 @@ print(bot.get_grades())
 #### 小例子：查询体育部未满人的课并保存为csv文件
 
 ```python
-from src.DHUPotatoPatch import DHUPotatoPatch
 import csv
+import asyncio
+
+with open("cookie", "r") as file:
+    cookie = file.read().strip()
+patch = DHUPotatoPatch(cookie=cookie)
 
 course_info = []
 
-for co in bot.search_courses_by_collage(3):
-    for cl in bot.search_courses_by_id(co["courseCode"], 81):
+tasks = []
+for co in await patch.search_courses_by_collage(3):
+    tasks.append(patch.search_courses_by_id(co["courseCode"], 81))
+
+results = await asyncio.gather(*tasks)
+
+for result in results:
+    for cl in result:
         if cl["maxNum"] > cl["admit"]:
             course_info.append(cl)
 
 headers = ['courseName', 'collage', 'courseCode', 'maxNum',
-           'admit', 'campus', 'teacher', 'week', 'time', 'location']
+            'admit', 'campus', 'teacher', 'week', 'time', 'location']
 
-with open('course_info.csv', 'w', newline='') as f:
-    writer = csv.DictWriter(f, fieldnames=headers)
+with open('course_info.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.DictWriter(f, headers)
     writer.writeheader()
+    for cl in course_info:
+        writer.writerow(cl)
 
-    for info in course_info:
-        writer.writerow(info)
+df = pd.read_csv('course_info.csv')
 
 print("done")
 
--- src/DHUPotatoPatch.py
+++ src/DHUPotatoPatch.py
@@ -1,5 +1,4 @@
 import httpx
-import asyncio
 from bs4 import BeautifulSoup
 
 param = "vpn-12-o2-jwgl.dhu.edu.cn"
@@ -13,33 +12,8 @@ async def post_request(url: str, headers: dict, payload: dict, params: str) -> d
 
 
 class DHUPotatoPatch:
-    """
-    A class representing a DHU Potato Patch, which provides various methods for searching courses, selecting courses,
-    removing courses, and retrieving grades.
-
-    Attributes:
-        headers (dict): The headers to be included in the HTTP requests.
-        semester (int): The current semester ID.
-
-    Methods:
-        search_courses_by_name: Search courses by name.
-        search_courses_by_id: Search courses by ID.
-        search_courses_by_collage: Search courses by collage.
-        select_course: Select a course.
-        remove_course: Remove a course.
-        get_grades: Get grades for a specific semester.
-        get_gpa: Get the GPA (Grade Point Average).
-        get_the_class_schedule: Get the class schedule.
-        get_current_semester: Get the current semester ID.
-    """
 
     def __init__(self, cookie: str):
-        """
-        Initializes a new instance of the DHUPotatoPatch class.
-
-        Args:
-            cookie (str): The cookie string used for authentication.
-        """
 
         self.headers = {
             "Cookie": cookie,
@@ -48,23 +22,6 @@ class DHUPotatoPatch:
         self.semester = self.get_current_semester()
 
     async def search_courses_by_name(self, courseName: str, termId: int = None) -> list:
-        """
-        Search courses by name.
-
-        Args:
-            courseName (str): The name of the course to search for.
-            termId (int, optional): The ID of the term to search in. Defaults to None.
-
-        Returns:
-            list: A list of dictionaries representing the search results, each containing the course name, course ID,
-                  credit, and organization name.
-        
-        Example:
-            ```python
-            DHUPotatoPatch.search_courses_by_name("计算机")
-            DHUPotatoPatch.search_courses_by_name("计算机", 81)
-            ```
-        """
 
         url = f"{BASE_URL}/PublicQuery/getSelectCourseTermList"
         payload = {
@@ -106,23 +63,6 @@ class DHUPotatoPatch:
         return courses
 
     async def search_courses_by_id(self, courseCode: str, termId=None) -> list:
-        """
-        Search courses by ID.
-
-        Args:
-            courseCode (str): The ID of the course to search for.
-            termId (int, optional): The ID of the term to search in. Defaults to None.
-
-        Returns:
-            list: A list of dictionaries representing the search results, each containing the course name, collage,
-                  course code, maximum number of students, admission number, campus, teacher, week, time, and location.
-
-        Example:
-            ```python
-            DHUPotatoPatch.search_courses_by_id("039981")
-            DHUPotatoPatch.search_courses_by_id("039981", 81)
-            ```
-        """
 
         url = f"{BASE_URL}/PublicQuery/getCourseTimeTableInfo"
         payload = {
@@ -154,21 +94,6 @@ class DHUPotatoPatch:
         return courseInfo
 
     async def search_courses_by_collage(self, orgnId: int) -> list:
-        """
-        Search courses by collage.
-
-        Args:
-            orgnId (int): The ID of the collage to search for.
-
-        Returns:
-            list: A list of dictionaries representing the search results, each containing the credit, course code,
-                  and course name.
-
-        Example:
-            ```python
-            DHUPotatoPatch.search_courses_by_collage(3)
-            ```
-        """
 
         url = f"{BASE_URL}/selectcourse/initSCByOrgn"
         payload = {
@@ -188,22 +113,6 @@ class DHUPotatoPatch:
         return courses
 
     async def select_course(self, courseId: str, needMaterial: bool = False) -> dict:
-        """
-        Select a course.
-
-        Args:
-            courseId (str): The ID of the class to select.
-            needMaterial (bool, optional): Whether materials are needed for the course. async Defaults to False.
-
-        Returns:
-            bool: True if the course selection is successful, False otherwise.
-
-        Example:
-            ```python
-            DHUPotatoPatch.select_course("269663")
-            DHUPotatoPatch.select_course("269663", True)
-            ```
-        """
 
         url = f"{BASE_URL}/selectcourse/scSubmit"
         payload = {
@@ -214,21 +123,6 @@ class DHUPotatoPatch:
         return await post_request(url, self.headers, payload, param)
 
     async def remove_course(self, courseCode: str, classNo: int) -> dict:
-        """
-        Remove a course.
-
-        Args:
-            courseCode (str): The ID of the course to remove.
-            classNo (int): The class number of the course to remove.
-
-        Returns:
-            bool: True if the course removal is successful, False otherwise.
-
-        Example:
-            ```python
-            DHUPotatoPatch.remove_course("039981", 1)
-            ```
-        """
 
         url = f"{BASE_URL}/selectcourse/cancelSC"
         payload = {
@@ -240,22 +134,6 @@ class DHUPotatoPatch:
         return await post_request(url, self.headers, payload, param)
 
     def get_grades(self, semester: int = None) -> list:
-        """
-        Get grades for a specific semester.
-
-        Args:
-            semester (int): The ID of the semester to retrieve grades for.
-
-        Returns:
-            list: A list of dictionaries representing the grades for each course, including the course name,
-                  course ID, credit, grade, and GPA.
-
-        Example:
-            ```python
-            DHUPotatoPatch.get_grades()
-            DHUPotatoPatch.get_grades(81)
-            ```
-        """
 
         url = f"{BASE_URL}/grade/gradeAnalysis/showPsersonalGrades"
         payload = {
@@ -263,57 +141,31 @@ class DHUPotatoPatch:
             "xh": "0"
         }
 
-        response = httpx.post(url, headers=self.headers, data=payload, params=param).json()
+        response = httpx.post(url, headers=self.headers,
+                              data=payload, params=param).json()
         response_list = response["list"][0]["courseGrades"]
 
         return [{"courseName": item["KCMC"], "credit": item["XF"], "grade": item["CJ"]} for item in response_list]
-    
+
     def get_gpa(self) -> list:
-        """
-        Get the GPA (Grade Point Average).
-
-        Returns:
-            list: A list of dictionaries representing the GPA for each semester, including the semester name
-                and GPA value.
-
-        Example:
-            ```python
-            DHUPotatoPatch.get_gpa()
-            ```
-        """
 
         url = f"{BASE_URL}/grade/student/showGradePoint"
         payload = {
             "type": 1
         }
 
-        response = httpx.post(url, headers=self.headers, data=payload, params=param).json()
+        response = httpx.post(url, headers=self.headers,
+                              data=payload, params=param).json()
 
         response_list = response["list"]
 
         return [{"averageGpa": item["ptjd"], "semester": item["xqbh"]} for item in response_list]
 
     def get_the_class_schedule(self):
-        """
-        Get the class schedule.
-
-        TODO: Add documentation for this method.
-        """
 
         pass
 
     def get_current_semester(self) -> int:
-        """
-        Get the current semester ID.
-
-        Returns:
-            int: The ID of the current semester.
-
-        Example:
-            ```python
-            DHUPotatoPatch.get_current_semester()
-            ```
-        """
 
         url = f"{BASE_URL}/common/semesterSS"
         payload = {
@@ -322,7 +174,8 @@ class DHUPotatoPatch:
         }
 
         try:
-            response = httpx.post(url=url, headers=self.headers, data=payload, params=param).json()
+            response = httpx.post(
+                url=url, headers=self.headers, data=payload, params=param).json()
         except:
             print("Invalid cookie! Please check that the cookie is correct.")
             exit(1)
